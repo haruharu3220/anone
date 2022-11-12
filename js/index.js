1bef997193a93d83cc10fe37fe70f8e28c9866c2
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-13 06:09:01 +0900

    TOPページのボタンをクリックしたとき遷移するように修正

---
+++ js/index.js
@@ -0,0 +1,17 @@
+
+$(".btn-memberSetting").on("click",function(){
+        console.log("設定ボタン押したよ");
+    location.href="../html/settings.html";
+});
+
+
+$(".btn-login").on("click",function(){
+    console.log("設定ボタン押したよ");
+location.href="../html/login.html";
+});
+
+
+$(".btn-usage").on("click",function(){
+    console.log("設定ボタン押したよ");
+location.href="../html/usage.html";
+});
