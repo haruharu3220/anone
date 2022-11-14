0fbe6f026332aaec0bac7af25463582bc1d3e706
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-15 03:13:52 +0900

    モーダルの作成

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
 $(".btn-comment").on("click",function(){
     console.log("コメントボタン押したよ");
-location.href="../html/history.html";
+
 });
 
 
@@ -18,3 +18,23 @@ $(".btn-history").on("click",function(){
     console.log("履歴ボタン押したよ");
 location.href="../html/history.html";
 });
+
+
+function popupImage() {
+    var popup = document.getElementById('js-popup');
+    if(!popup) return;
+    var blackBg = document.getElementById('js-black-bg');
+    var closeBtn = document.getElementById('js-close-btn');
+    var showBtn = document.getElementById('js-show-popup');
+  
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+    function closePopUp(elem) {
+      if(!elem) return;
+      elem.addEventListener('click', function() {
+        popup.classList.toggle('is-show');
+      });
+    }
+  }
+  popupImage();
