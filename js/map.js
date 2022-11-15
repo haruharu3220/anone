8ffaed4e01dc306183b4ceb82ffabbfcf245d25a
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-15 12:34:57 +0900

    モーダルの項目を修正

--- js/map.js
+++ js/map.js
@@ -7,42 +7,43 @@
 // 
 // 
 
-$(".btn-comment").on("click",function(){
+$(".btn-comment").on("click", function () {
     console.log("コメントボタン押したよ");
 
 });
 
 
-$(".btn-logout").on("click",function(){
+$(".btn-logout").on("click", function () {
     console.log("ログアウトボタン押したよ");
-location.href="../html/login.html";
+    location.href = "../html/login.html";
 });
 
-$(".btn-returnTop").on("click",function(){
+$(".btn-returnTop").on("click", function () {
     console.log("TOPに戻るボタン押したよ");
-location.href="../index.html";
+    location.href = "../index.html";
 });
 
-$(".btn-history").on("click",function(){
+$(".btn-history").on("click", function () {
     console.log("履歴ボタン押したよ");
-location.href="../html/history.html";
+    location.href = "../html/history.html";
 });
 
 
 function popupImage() {
     var popup = document.getElementById('js-popup');
-    if(!popup) return;
+    if (!popup) return;
     var blackBg = document.getElementById('js-black-bg');
     var closeBtn = document.getElementById('js-close-btn');
     var showBtn = document.getElementById('js-show-popup');
-  
+
     closePopUp(blackBg);
     closePopUp(closeBtn);
     closePopUp(showBtn);
     function closePopUp(elem) {
-      if(!elem) return;
-      elem.addEventListener('click', function() {
-        popup.classList.toggle('is-show'); //https://qiita.com/tomokichi_ruby/items/2460c5902d19b81cace5
-    }
+        if (!elem) return;
+        elem.addEventListener('click', function () {
+            popup.classList.toggle('is-show'); //https://qiita.com/tomokichi_ruby/items/2460c5902d19b81cace5
+        });
   }
-  popupImage();
+}
+popupImage();
