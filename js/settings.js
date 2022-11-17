86af823497e68b36c7b3a20b5e382f5242149e55
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-17 21:52:07 +0900

    キャラ選択画面作成

--- js/settings.js
+++ js/settings.js
@@ -70,70 +70,22 @@ $(".btn-returnTop").on("click", function () {
   location.href = "../index.html";
 });
 
+
 $(".btn-login").on("click", function () {
   console.log("loginボタン押したよ");
+
+
+
+
   location.href = "../html/map.html";
 });
 
 
-const FORM = $("form"); // set form or other element here
-const TYPES = ["input[type=text], input[type=submit]"]; // set which elements get targeted by the focus
-const FOCUS = $(".focus"); // focus element
-
-// function for positioning the div
-function position(e) {
-  // get position
-  var props = {
-    top: e.offset().top,
-    left: e.offset().left,
-    width: e.outerWidth(),
-    height: e.outerHeight(),
-    radius: parseInt(e.css("border-radius"))
-  };
-
-  // set position
-  FOCUS.css({
-    top: props.top,
-    left: props.left,
-    width: props.width,
-    height: props.height,
-    "border-radius": props.radius
-  });
-
-  FOCUS.fadeIn(200);
-}
-
-FORM.find(TYPES.join()).each(function (i) {
-  // when clicking an input defined in TYPES
-  $(this).focus(function () {
-    el = $(this);
-
-    // adapt size/position when resizing browser
-    $(window).resize(function () {
-      position(el);
-    });
-
-    position(el);
-  });
-});
-
-FORM.on("focusout", function (e) {
-  setTimeout(function () {
-    if (!e.delegateTarget.contains(document.activeElement)) {
-      FOCUS.fadeOut(200);
-    }
-  }, 0);
-});
-
-
-
-//Tips
-//if (!localStorage.hasOwnProperty("mumberData")) {
-
-
 
 $(".btn-login").on("click", function () {
   console.log("loginボタン押したよ");
+  const text =  $("[name=ueserName] option:selected").text();
+  localStorage.setItem("SelectedNunber", text);
   location.href = "../html/map.html";
 });
 
@@ -143,17 +95,22 @@ $(".fa-square-plus").on("click", function () {
   console.log("家族追加ボタン押したよ");
 
   //下に名前を表示
-  $('ul').append("<li>" + $(".inputMenberInput").val() + "</li>");
+  $('ul').append("<li>" + $("[name=ueserName] option:selected").text() + "</li>");
 
   // https://www.webopixel.net/javascript/216.html
-  var i = 1;
+  // var i = 1;
+  // $('li').each(function () {
+  //   $(this).addClass('style-li' + i);
+  //   i++;
+  // }
   $('li').each(function () {
-    $(this).addClass('style-li' + i);
-    i++;
-  });
+      $(this).addClass('style-li');
+  })
 
 
-  $('ul').append("<li><div><i class=\"fa-regular fa-pen-to-square fa-xl\"></i></div></li>");
+
+// ボツになったアイコンたち
+  // $('ul').append("<li><div><i class=\"fa-regular fa-pen-to-square fa-xl\"></i></div></li>");
   // $('ul').append("<li><div><i class=\"fa-regular fa-circle-xmark fa-xl\"></i></div></li>");
 
   // https://iwb.jp/jquery-append-dom-nest/
@@ -180,22 +137,26 @@ $(".fa-square-plus").on("click", function () {
   }
 });
 
+
+
+
+
 //キャラ選択ボタン→各キャラ分作成必要あり
-$("body").on("click", ".style-li1", function () {
+$("body").on("click", ".style-li", function () {
 
-  console.log("押したよtest|||" + $(".style-li1").text());
-  const jsonData = localStorage.getItem("memo");
-  const data = JSON.parse(jsonData);
-  console.log(data);
-  console.log(data.length);
-  for (let i = 0; i < data.length; i++) {
-    // console.log("ここ");
-    if (data[i] === $(".style-li1").text()) {
-      localStorage.setItem("selectMumber", i);
+  // console.log("押したよtest|||" + $(".style-li1").text());
+  // const jsonData = localStorage.getItem("memo");
+  // const data = JSON.parse(jsonData);
+  // console.log(data);
+  // console.log(data.length);
+  // for (let i = 0; i < data.length; i++) {
+  //   // console.log("ここ");
+  //   if (data[i] === $(".style-li1").text()) {
+  //     localStorage.setItem("selectMumber", i);
 
-    }
-  }
+  //   }
+  // }
   //ログインモーダル表示
   loginModal.style.display = 'block';
-  $(".selectedMumer").append($(".style-li1").text());
+   $(".selectedMumer").append($("[name=ueserName] option:selected").text());
 });
