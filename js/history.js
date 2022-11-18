69b104150a4f40f52d45951696fce58df11fed7a
haruharu3220 <hayato.314.nico@gmail.com>
2022-11-18 21:45:50 +0900

    履歴画面を作成

--- js/history.js
+++ js/history.js
@@ -1,9 +1,14 @@
 $(".btn-returnMap").on("click", function () {
-    console.log("ログアウトボタン押したよ");
+    console.log("マップに戻る押したよ");
     location.href = "../html/map.html";
 });
 
 $(".btn-returnTop").on("click", function () {
     console.log("TOPに戻るボタン押したよ");
     location.href = "../index.html";
+});
+
+$("#btn-usage").on("click",function(){
+    console.log("使い方へ押したよ");
+location.href="../html/usage.html";
 });
